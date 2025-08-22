2f4cd074f8d571ccac0c47fafe2ababfd23880ae
Tyler <tylerkp27@ausdk12.org>
2025-08-22 14:20:21 -0700

    Friday

--- BodyOfWater.java
+++ BodyOfWater.java
@@ -5,21 +5,32 @@ public class BodyOfWater {
     private boolean isSaltWater;
 
     public BodyOfWater(String n, int l, int d, boolean i){
-        n = name;
-        l = largestDiameter;
-        d = avgDepth;
-        i = isSaltWater;
+        name= n;
+        largestDiameter=l;
+        avgDepth=d;
+        isSaltWater=i;
     
     }
     public BodyOfWater(String n, int d){
-        n = name;
-        d = avgDepth;
+        name=n;
+        avgDepth=d;
 
     }
     public void setName(String n){
-        name = n;
+       name = n;
     }
-    public void setlargestDiameter(String l){
+    public void setlargestDiameter(int l){
         largestDiameter = l;
     }
+    
+    public void printState(){
+        System.out.println("Name: " + name);
+
+        System.out.println("Diameter: " + largestDiameter);
+
+        System.out.println("avgDepth: "+ avgDepth);
+
+        System.out.println("isSaltWater: "+ isSaltWater);
+    }
+
 }
--- BodyOfWaterMain.java
+++ BodyOfWaterMain.java
@@ -1,6 +1,10 @@
 public class BodyOfWaterMain {
     public static void main(String[] args) {
-        BodyOfWater b = new BodyOfWater();
-        System.out.println("here");
+        BodyOfWater b = new BodyOfWater("Sean", 500, 300,true);
+        BodyOfWater b1 = new BodyOfWater("Tyler", 300);
+        b.printState();
+        b.setName("Green");
+        b.printState();
+        b1.printState();
     }
 }
