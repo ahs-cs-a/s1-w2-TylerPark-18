a501afcd647436011ce7da259b89f29e43a7d8bb
Tyler <tylerkp27@ausdk12.org>
2025-08-22 13:45:13 -0700

    work today 1

--- BodyOfWater.java
+++ BodyOfWater.java
@@ -1,1 +1,20 @@
+public class BodyOfWater {
+    private String name;
+    private int largestDiameter;
+    private int avgDepth;
+    private boolean isSaltWater;
 
+    public BodyOfWater(String n, int l, int d, boolean i){
+        n = name;
+        l = largestDiameter;
+        d = avgDepth;
+        i = isSaltWater;
+    
+    }
+    public BodyOfWater(String n, int d){
+        n = name;
+        d = avgDepth;
+
+    }
+    public void Na 
+}
